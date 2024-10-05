f0fc3aeda1a3cc672b984d7514c1667109007978
Gorciu <122084593+gorciu-official@users.noreply.github.com>
2024-10-05 15:49:00 +0200

    innertext is safer, better link

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
-// Example code to fetch the latest release info from GitHub API
-
-const latestReleaseUrl = 'https://api.github.com/repos/your-repo/rawberry/releases/latest';
-
-fetch(latestReleaseUrl)
-    .then(response => response.json())
-    .then(data => {
-        document.querySelector('.download button').innerHTML = data.name;
-        document.querySelector('.download button').onclick = () => {
-            window.location.href = data.zipball_url;
-        };
-    })
-    .catch(error => console.error('Error fetching latest release:', error));
+document.querySelector('.download button').innerText = 'Download';
+document.querySelector('.download button').onclick = () => {
+    window.location.href = 'https://github.com/RawBerryTeam/RawBerry/releases/latest';
+};
